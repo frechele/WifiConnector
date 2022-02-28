f52fa78531ca1db163925b9644f347da96344cfe
Junyeong Park <jyp10987@gmail.com>
2022-03-01 00:59:02 +0900

    feat(core): QR 생성 함수 구현

--- lib/core.dart
+++ lib/core.dart
@@ -1,5 +1,26 @@
+import 'dart:typed_data';
+import 'dart:ui';
+import 'dart:convert';
+
 import 'package:easy_wifi_connect/easy_wifi_connect.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 Future<String?> ConnectWifi({required String ssid, required String password}) {
   return EasyWifiConnect.connectWifi(ssid, password);
+}
+
+Future<Uint8List> GenerateQR(Object object) async {
+  final jsonStr = jsonEncode(object);
+
+  final image = await QrPainter(
+    data: jsonStr,
+    version: QrVersions.auto,
+    gapless: false,
+    color: Colors.black,
+    emptyColor: Colors.white
+  ).toImage(300);
+
+  final buf = await image.toByteData(format: ImageByteFormat.png);
+  return buf!.buffer.asUint8List();
 }
