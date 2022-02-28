4b4e6cfec1b128a82d8ea94be1c4cf9748e6bf33
Junyeong Park <jyp10987@gmail.com>
2022-03-01 01:28:44 +0900

    refactor: GenerateQR rㅐ선

--- lib/core.dart
+++ lib/core.dart
@@ -1,26 +1,23 @@
-import 'dart:typed_data';
-import 'dart:ui';
 import 'dart:convert';
+import 'dart:ui';
 
 import 'package:easy_wifi_connect/easy_wifi_connect.dart';
-import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 Future<String?> ConnectWifi({required String ssid, required String password}) {
   return EasyWifiConnect.connectWifi(ssid, password);
 }
 
-Future<Uint8List> GenerateQR(Object object) async {
+Future<Image> GenerateQR(Object object) async {
   final jsonStr = jsonEncode(object);
 
-  final image = await QrPainter(
+  final painter = QrPainter(
     data: jsonStr,
     version: QrVersions.auto,
     gapless: false,
-    color: Colors.black,
-    emptyColor: Colors.white
-  ).toImage(300);
+    color: const Color(0xFF000000),
+    emptyColor: const Color(0xFFFFFFFF)
+  );
 
-  final buf = await image.toByteData(format: ImageByteFormat.png);
-  return buf!.buffer.asUint8List();
+  return await painter.toImage(300);
 }
