750503da88b1c8b31b19b0aa8045c4050aaef35c
Junyeong Park <jyp10987@gmail.com>
2022-02-28 21:50:21 +0900

    fix: fix build error

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wifi_connector/main.dart';
+import 'package:wifi_connector_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
